50cdfa9a1ec0c765544eff1e20485c537ec8e4e3
diaznakh <diaznakh@gmail.com>
2026-01-21 14:56:19 +0530

    Leetcode: Course Scheduling 2 Problem

---
+++ Topics/Graphs/course_schedule_2.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        vector<int> indeg(numCourses, 0);
+
+        vector<int> ans;
+        vector<int> ans1;
+        // converting to adjacency list first and calculatin indegree of each
+        // course that is the number of incoming edges
+
+        for (auto& p : prerequisites) {
+            int course = p[0];
+            int prereq = p[1];
+            adj[prereq].push_back(course);
+            indeg[course]++;
+        }
+
+        queue<int> q;
+
+        // if there exists a course with no prerequisites take it imediately
+        for (int i = 0; i < numCourses; i++) {
+            if (indeg[i] == 0) {
+                q.push(i);
+                ans.push_back(i);
+            }
+        }
+
+        // history of completed courses
+        int completed = 0;
+
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            completed++;
+
+            for (int next : adj[node]) {
+                indeg[next]--;
+                if (indeg[next] == 0) {
+                    q.push(next);
+                    ans.push_back(next);
+                }
+            }
+        }
+
+        if (completed == numCourses) {
+            return ans;
+        }
+
+        return ans1;
+    }
+};
